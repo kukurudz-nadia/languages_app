45df070b98494d47cef50bdb15b56079996abc81
Kukurudz Nadiia <94442918+kukurudz-nadia@users.noreply.github.com>
2023-03-17 12:18:29 +0200

    business logic (#5)
     added searching by name

---
+++ app/controllers/languages_controller.rb
@@ -0,0 +1,14 @@
+class LanguagesController < ApplicationController
+  def index
+    @languages = Language.all
+  end
+
+  def show
+    @language = Language.find(params[:id])
+  end
+
+  def search_by_name
+    @search_query = params[:name]
+    @languages = Language.where(name: @search_query)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :languages, only: [:index, :show]
+  get "/search", to: "languages#search_by_name", as: "search_by_name"
+  root "homes#index"
 end
