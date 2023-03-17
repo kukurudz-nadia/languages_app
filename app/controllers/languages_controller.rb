9c079a14934be8eaff210ef2b71d8e925a292bf4
Kukurudz Nadiia <94442918+kukurudz-nadia@users.noreply.github.com>
2023-03-17 16:12:04 +0200

    search by designer
     add searching by designer

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -10,9 +10,14 @@ class LanguagesController < ApplicationController
     @search_query = params[:name]
     @languages = Language.where("name ILIKE ?", @search_query)
     language = @languages.first
-    if language.nil?
+    if language.nil? && !@search_query.nil?
       reversed_name = @search_query.split(" ").reverse.join(" ")
       @languages = Language.where("name ILIKE ?", reversed_name)
     end
+  end
+
+  def search_by_designer
+    @search_query = params[:designed_by]
+    @languages = Language.where("designed_by ILIKE ?", "%#{@search_query}%")
   end
 end
