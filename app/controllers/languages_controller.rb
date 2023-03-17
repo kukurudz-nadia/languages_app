fac138c7c6f5305a483d39278b2ddd51c2a0b8ad
Kukurudz Nadiia <94442918+kukurudz-nadia@users.noreply.github.com>
2023-03-17 15:33:26 +0200

    added searching by name

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -8,6 +8,11 @@ class LanguagesController < ApplicationController
 
   def search_by_name
     @search_query = params[:name]
-    @languages = Language.where(name: @search_query)
+    @languages = Language.where("name ILIKE ?", @search_query)
+    language = @languages.first
+    if language.nil?
+      reversed_name = @search_query.split(" ").reverse.join(" ")
+      @languages = Language.where("name ILIKE ?", reversed_name)
+    end
   end
 end
