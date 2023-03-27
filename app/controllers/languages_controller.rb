37866e7133b5b2ee7eb063c450bec3e0afa95857
Kukurudz Nadiia <94442918+kukurudz-nadia@users.noreply.github.com>
2023-03-27 14:03:25 +0300

    added render (#26)

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -9,50 +9,19 @@ class LanguagesController < ApplicationController
 
   def search_by_name
     @search_query = params[:name]
-    @languages = Language.where("name ILIKE ?", @search_query)
-    language = @languages.first
-    if @languages.blank? && @search_query.present?
-      reversed_name = @search_query.split.reverse.join(" ")
-      @languages = Language.where("name ILIKE ?", reversed_name)
-    end
+    @languages = Language.search_by_name(@search_query)
   end
 
   def search_by_type_or_designer
     @search_query = params[:type]
     if @search_query.present?
-      params_array = @search_query.split
-      type, designed_by = match_params(params_array)
-
-      negative_element = params_array.find { |elem| elem.start_with?("-") }
+      type, designed_by = Language.match_params(@search_query.split)
+      negative_element = params[:type].split.find { |elem| elem.start_with?("-") }
       if negative_element.present?
-        negative_search(negative_element, type, designed_by)
+        @languages = Language.negative_search(negative_element, type, designed_by)
       else
-        @languages = Language.where("type ILIKE ? AND designed_by ILIKE ?", "%#{type.join(" ")}%", "%#{designed_by.join(" ")}%")
+        @languages = Language.search_by_type_and_designed_by(type, designed_by)
       end
-    else
-      @languages = Language.none
-    end
-  end
-
-  def match_params(params_array)
-    type, designed_by = [], []
-    params_array.each do |param|
-      if Language.where("type ILIKE ?", "%#{param}%").exists?
-        type << param
-      elsif Language.where("designed_by ILIKE ?", "%#{param}%").exists?
-        designed_by << param
-      end
-    end
-    return type, designed_by
-  end
-
-  def negative_search(negative_element, type, designed_by)
-    changed_neg_el = negative_element.delete_prefix("-")
-
-    if Language.where("type ILIKE ?", "%#{changed_neg_el}%").exists?
-      @languages = Language.where("type ILIKE ? AND designed_by ILIKE ? AND type NOT ILIKE ?", "%#{type.join(" ")}%", "%#{designed_by.join(" ")}%", "%#{changed_neg_el}%")
-    elsif Language.where("designed_by ILIKE ?", "%#{changed_neg_el}%").exists?
-      @languages = Language.where("type ILIKE ? AND designed_by ILIKE ? AND designed_by NOT ILIKE ?", "%#{type.join(" ")}%", "%#{designed_by.join(" ")}%", "%#{changed_neg_el}%")
     else
       @languages = Language.none
     end
